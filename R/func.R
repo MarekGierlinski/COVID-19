fd9e095b477ba155e28221444515e4f8631b36f6
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2020-03-15 13:02:16 +0000

    Drake

---
+++ R/func.R
@@ -0,0 +1,112 @@
+countries_eu <- c("Italy", "United Kingdom", "France", "Germany", "Netherlands", "Sweden", "Switzerland", "Spain")
+shapes <- c(15:18, 0:14)
+cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "grey60", "black")
+
+
+read_covid <- function() {
+  yesterday <- Sys.Date() - 1
+  url <- glue("https://ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-{yesterday}.xls")
+  tmp <- tempfile()
+  download.file(url, tmp, mode="wb")
+  read_excel(tmp)
+}
+
+process_covid <- function(cvd) {
+  cvd %>% 
+    mutate(CountryExp = recode(CountryExp, `United kingdom` = "United Kingdom")) %>% # spelling mistake
+    mutate(date = as.Date(DateRep, format="%d/%m/%Y")) %>% 
+    rename(country = CountryExp, new_cases = NewConfCases) %>% 
+    group_by(country) %>% 
+    arrange(date) %>% 
+    mutate(tot_cases = sum(new_cases), cases = cumsum(new_cases)) %>% 
+    mutate(deaths = cumsum(NewDeaths)) %>% 
+    ungroup() %>% 
+    filter(tot_cases > 200 & country != "Cases on an international conveyance Japan") %>% 
+    mutate(country = recode(country, "United States of America" = "United States"))
+}
+
+basic_plot <- function(d, x="date", y="cases", xlab="Date", ylab="Cases", palette=cbPalette, shps=shapes, point.size=1) {
+  brks <- rep(c(1, 2, 5), 5) * 10^sort(rep(0:4,3))
+  d %>% 
+    ggplot(aes_string(x=x, y=y, colour="country", shape="country", group="country")) +
+    theme_bw() +
+    theme(
+      panel.grid.minor = element_blank()
+    ) +
+    geom_line() +
+    geom_point(size=point.size) +
+    scale_colour_manual(values=palette) +
+    scale_shape_manual(values=shps) +
+    scale_y_log10(breaks=brks) +
+    labs(x=xlab, y=ylab)
+}
+
+shift_days <- function(d, shifts, what="cases", val.min=100) {
+  d %>% 
+    filter(!!what > val.min) %>% 
+    left_join(shifts, by="country") %>% 
+    mutate(days = as.integer(date) - shift - ref)
+}
+
+plot_shifted <- function(cvd, what="cases", val.min=100) {
+  shifts <- linear_shifts(cvd, what=what, val.min=val.min)
+  cvd %>% 
+    mutate(value = !!sym(what)) %>% 
+    filter(value > val.min) %>% 
+    left_join(shifts, by="country") %>% 
+    mutate(days = as.integer(date) - shift - ref) %>% 
+    basic_plot(x="days", y="value", xlab="Normalized day", ylab=what)
+}
+
+linear_shifts <- function(cvd, what="cases", base_country = "Italy", val.min=100) {
+  dat <- cvd %>%
+    mutate(value = !!sym(what)) %>% 
+    filter(value > val.min & value < 10000) %>%
+    mutate(lval = log2(value), day = as.integer(date)) %>% 
+    select(country, lval, day)
+  
+  finv <- lm(day ~ lval, data=filter(dat, country==base_country))
+  ref_day0 <- finv$coefficients[1]
+  ref_slope <- finv$coefficients[2]
+  ref_10000 <- predict(finv, data.frame(lval = log2(10000)))
+  
+  shifts <- dat %>% 
+    group_by(country) %>% 
+    summarise(shift = mean(day - ref_slope * lval) - ref_day0) %>% 
+    mutate(date10000 = as.Date(ref_10000 + shift, origin=as.Date("1970/01/01"))) %>% 
+    arrange(date10000) %>% 
+    mutate(ref = ref_day0)
+}
+
+get_doubling_times <- function(cvd, what="cases", val.min=100) {
+  cvd %>% 
+    mutate(value = !!sym(what)) %>% 
+    filter(value > val.min & cases < 10000 & country != "China") %>% 
+    mutate(lval = log2(value), day = as.integer(date)) %>% 
+    group_by(country) %>%
+    group_split() %>% 
+    map_dfr(function(w) {
+      if(nrow(w) < 5) return(NULL)
+      fit <-  lm(day ~ lval, data=w)
+      coef <- summary(fit)$coefficients
+      conf <- confint(fit)
+      tibble(
+        country = first(w$country),
+        slope = coef[2, 1],
+        lo = conf[2, 1],
+        up = conf[2, 2]
+      )
+    }) %>% 
+    arrange(slope) %>% 
+    mutate(country = as_factor(country))
+}
+
+plot_doubling_times <- function(cvd, what="cases", val.min=100, lab="Reported cases") {
+  get_doubling_times(cvd, what, val.min=val.min) %>% 
+    ggplot(aes(x=country, y=slope, ymin=lo, ymax=up)) +
+    theme_bw() +
+    geom_errorbar(width=0.3) +
+    geom_point() +
+    coord_flip() +
+    labs(x=NULL, y=glue::glue("{lab} doubling time (day)"))
+}
---
+++ _drake.R
@@ -0,0 +1,56 @@
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(readxl)
+  library(glue)
+  library(broom)
+  library(drake)
+})
+
+source("R/func.R")
+
+if(!dir.exists("fig")) dir.create("fig")
+
+read_data <- drake_plan(
+  covid_raw = read_covid(),
+  covid = process_covid(covid_raw),
+  covid_sel = covid %>% filter(country %in% countries_eu) %>% filter(cases > 0) %>% mutate(country = factor(country, levels=countries_eu))
+)
+
+find_shifts <- drake_plan(
+  shifts_cases = linear_shifts(covid, "cases"),
+  shifts_deaths = linear_shifts(covid, "deaths",  val.min=10)
+)
+
+plots <- drake_plan(
+  plot_cases = basic_plot(covid_sel),
+  plot_shifted_cases = plot_shifted(covid_sel, what="cases"),
+  plot_shifted_deaths = plot_shifted(covid_sel, what="deaths", val.min=10),
+  plot_doubling_cases = plot_doubling_times(covid, "cases", val.min=100, lab="Reported cases"),
+  plot_doubling_deaths = plot_doubling_times(covid, "deaths", val.min=10, lab="Reported deaths")
+)
+
+figs <- plots %>% 
+  rename(name = target) %>% 
+  select(-command) %>% 
+  mutate(
+    obj = rlang::syms(name),
+    filename = paste0("fig/", str_remove(name, "plot_"), ".png")
+  )
+
+save_figures <- drake_plan(
+  figures = target(
+    command = ggsave(filename, obj, device="png", width=6, height=3),
+    transform = map(.data = !!figs)
+  )
+)
+
+
+
+plan <- bind_rows(
+  read_data,
+  find_shifts,
+  plots,
+  save_figures
+)
+
+cfg <- drake_config(plan)
